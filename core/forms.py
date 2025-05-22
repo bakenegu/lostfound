acea39f9f6b58c0b83680863c17084ddf984bcab
Biruk Tadesse <bakenegu@gmail.com>
2025-05-22 23:10:02 +0300

    error solved

--- core/forms.py
+++ core/forms.py
@@ -5,6 +5,10 @@ class LostItemForm(forms.ModelForm):
     class Meta:
         model = LostItem
         fields = ['title', 'description', 'date_lost', 'location', 'contact_info']
+        widgets = {
+            'date_lost': forms.DateInput(attrs={'type': 'date'}),
+        }
+
 
 class FoundItemForm(forms.ModelForm):
     class Meta:
