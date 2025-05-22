41d12a37f65dbd8331e4e4e47a6aa207472b817e
Abel <abelgezahegn61@gmail.com>
2025-05-22 21:16:15 +0300

    Initial commit

---
+++ core/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import LostItem, FoundItem
+
+class LostItemForm(forms.ModelForm):
+    class Meta:
+        model = LostItem
+        fields = ['title', 'description', 'date_lost', 'location', 'contact_info']
+
+class FoundItemForm(forms.ModelForm):
+    class Meta:
+        model = FoundItem
+        fields = ['title', 'description', 'photo', 'location']
---
+++ core/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('staff', 'Staff'),
+        ('admin', 'Admin'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.role})"
+
+class LostItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date_lost = models.DateField()
+    location = models.CharField(max_length=100)
+    contact_info = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class FoundItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='found_photos/')
+    location = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
